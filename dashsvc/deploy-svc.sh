340161752fb295818379ea36c9edf955dfab098c
Dave Dopson <ddopson@gmail.com>
2016-02-09 18:58:42 -0700

    dev mode should use full HighStock source

--- dashsvc/deploy-svc.sh
+++ dashsvc/deploy-svc.sh
@@ -1,5 +1,4 @@
 set -e
-./deploy-code.sh
 
 gcloud --project renature-dashboard preview app modules delete default --version $(gcloud --project renature-dashboard preview app modules list | perl -pe 's/ +/\t/g' | cut -f2 | grep 2015 | head -n1) --quiet
 gcloud --project renature-dashboard preview app deploy --promote app.yaml --quiet
