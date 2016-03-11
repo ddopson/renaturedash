f183648f2489a9fff01f4520c4418e51e179d7ce
Dave Dopson <ddopson@gmail.com>
2016-03-11 09:45:08 -0700

    blockfile

--- scraper/entrypoint.sh
+++ scraper/entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+BLOCKFILE=/home/ddopson/renaturedash/scraper/LOCK
+
+if [ -e $BLOCKFILE ]; then
+  echo 'Blocked from running'
+  exit 1
+fi
+
 /home/ddopson/renaturedash/scraper/sync.sh &>> /home/ddopson/renaturedash/scraper/log/$(date +%s).txt
